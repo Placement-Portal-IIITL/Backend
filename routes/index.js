7d3925e543fe73aae3b024ded69d2a057c666211
Rohit Kumar <rohitworks200@gmail.com>
2022-11-03 15:48:46 +0530

    Removed address direct apis

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,11 @@ const accountRoutes = require("./account");
 const departmentRoutes = require("./department");
 const courseRoutes = require("./course");
 const studentRoutes = require("./student");
-const addressRoutes = require("./address");
 
 router.use("/", authRoutes);
 router.use("/", accountRoutes);
 router.use("/", departmentRoutes);
 router.use("/", courseRoutes);
 router.use("/", studentRoutes);
-router.use("/", addressRoutes);
 
 module.exports = router;
